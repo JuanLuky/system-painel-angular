86e2af83d779ef8db556b66c2d7843231e2e5094
Juan Carlos <86580442+JuanLuky@users.noreply.github.com>
2025-05-22 22:57:44 -0300

    fix: roolback

--- src/app/pages/painel-senha/painel-senha.component.ts
+++ src/app/pages/painel-senha/painel-senha.component.ts
@@ -22,7 +22,7 @@ export class PainelSenhaComponent {
 
   conectarWebSocket(): void {
     this.stompClient = new Client({
-      webSocketFactory: () => new SockJS('https://localhost:8080/ws'),
+      webSocketFactory: () => new SockJS('http://localhost:8080/ws'),
       debug: str => console.log(str),
     });
 
