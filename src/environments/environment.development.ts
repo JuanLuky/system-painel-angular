9b7c2bb0a4f841e85762a894788d68a424a02114
juan10 <juandev33@gmail.com>
2025-06-23 21:22:02 -0300

    fix: url render backend

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  API_URL: 'http://localhost:8080/api', // URL da API em desenvolvimento
-  // API_URL: 'https://spring-painel-senha.onrender.com/api', // URL da API em produção
+  // API_URL: 'http://localhost:8080/api', // URL da API em desenvolvimento
+  API_URL: 'https://spring-painel-senha.onrender.com/api', // URL da API em produção
 };
